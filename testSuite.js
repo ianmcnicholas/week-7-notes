502e807b66c9ed295de3292b7077a46b161e8532
Raf Swiderski <rafswid@icloud.com>
2021-03-15 16:09:38 +0000

    test suite

--- testSuite.js
+++ testSuite.js
@@ -1,3 +1,11 @@
-it('does 2 + 2', function() {
-  expect(2 + 3).toEqual(4)
+describe('does sums', function() {
+  it('does 2 + 2', function() {
+    expect(2 + 2).toEqual(4)
+  });
+
+  it('does 2 + 3', function() {
+    expect(2 + 3).toNotEqual(4)
+  });
+
 });
+  
